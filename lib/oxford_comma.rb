e329c4b3ed299fa6f284d5c5ad72a00191d6b154
Jason Basuil <basuilj@gmail.com>
2018-11-28 06:34:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  count = array.count
+  if count == 1 
+    array.join
+  elsif count == 2 
+    array.join(" and ")
+  else
+    array.insert(-2, "and")
+    array.join
+    
+  end 
 end
